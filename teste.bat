bdcde81f8d58cba25b39660b1f12b30532041d4f
piradata <guilhermefercar@gmail.com>
2019-03-03 21:50:10 -0300

    automate env creation

---
+++ teste.bat
@@ -0,0 +1,23 @@
+@echo off
+echo.
+echo Hallo meine Freund, this will configure everything in the virtual env XD
+echo.
+pause XD
+echo.
+echo initiate of creation virtual environment
+echo.
+python -m venv env
+echo virtual environment created
+echo.
+call .\env\Scripts\activate
+echo virtual env active
+echo.
+echo initiate search for dependances
+echo.
+pip install -r requirements.txt
+echo.
+echo dependances installed
+echo.
+echo everything done, bailing out
+echo.
+pause XD
